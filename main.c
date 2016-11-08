cceea60fdb0dcc61ab476d8a46b5e21e3aabd533
Oleksandr Zelentsov <oleksandrzelentsov@gmail.com>
2016-11-08 19:27:25 +0100

    module-wise development, cli args

--- main.c
+++ main.c
@@ -2,13 +2,59 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
+#include <sys/stat.h>
+
 #include "levenstein_distance.h"
+
+#ifndef between
+#define between(A,X,Y) ((A) < (Y) && (A) >= (X))
+#endif
 
 static char* bin_filename;
 
-void error(char* message)
+void error(char* message);
+void debug(char* message);
+void usage();
+void print_distance(char* a, char* b);
+int index_of_string_in_strings(char* strings[], int strings_length, char* string);
+
+int main(int argc, char* argv[])
 {
-    printf("Błąd: %s.\n", message);
+    // enabling Polish characters
+    setlocale(LC_ALL, "");
+    // setting global bin_filename variable
+    bin_filename = argv[0];
+    int filename_idx = index_of_string_in_strings(argv, argc, "--filename") + 1;
+    char* input_filename = (between(filename_idx, 0, argc)) ?
+                           argv[filename_idx] :
+                           "./lwords.txt";
+    /* if we don't have enough arguments,
+     * check the file, or
+     * end program with an error message
+     * as well as the typical usage message */
+    if (argc < 3)
+    {
+        struct stat buf;
+        if (stat(input_filename, &buf))
+        {
+            // todo implement new behavior
+            printf("here will be the behavior with file input\n");
+            return 0;
+        }
+        else
+        {
+            error("brak argumentów");
+            usage();
+            return 1;
+        }
+    }
+    else
+    {
+        // copying words to variables
+        char* a = argv[1];
+        char* b = argv[2];
+        print_distance(a, b);
+    }
 }
 
 void usage()
@@ -25,26 +71,3 @@ void print_distance(char* a, char* b)
                        strlen(b));
     printf("LD(%s,\n   %s) = %i\n", a, b, d);
 }
-
-int main(int argc, char* argv[])
-{
-    // enabling Polish characters
-    setlocale(LC_ALL, "");
-    // setting global bin_filename variable
-    bin_filename = argv[0];
-    /* if we don't have enough arguments,
-     * end program with an error message
-     * as well as the typical usage message
-     */
-    if (argc < 3)
-    {
-        error("brak argumentów");
-        usage();
-        return 1;
-    }
-    // copying words to variables
-    char* a = argv[1];
-    char* b = argv[2];
-    print_distance(a, b);
-}
-
