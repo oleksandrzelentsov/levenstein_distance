a866a9b4d9dfadd8d9cdc0c39f37458c046ff065
Oleksandr Zelentsov <oleksandrzelentsov@gmail.com>
2016-11-12 16:05:22 +0100

    implementation of test cases

---
+++ tests/resources/levenstein_distance.py
@@ -0,0 +1,18 @@
+from subprocess import call, Popen, check_output
+from robot.libraries.BuiltIn import BuiltIn
+from robot.api.deco import keyword
+from re import findall
+
+
+_get_robot_var = BuiltIn().get_variable_value
+_log = BuiltIn().log
+_log_to_console = BuiltIn().log_to_console
+
+
+@keyword('application returns')
+def application_returns(arg1, arg2, r):
+    _log_to_console('checking if LD({}, {}) = {}'.format(arg1, arg2, r))
+    output = (check_output([_get_robot_var('${APP_EXECUTABLE_NAME}'), arg1, arg2])).decode('utf-8')
+    output = output.replace(' ', '').replace('\n', '')
+    temp = findall(r'\(([\w\d]*)\,([\w\d]*)\)\=(\d*)', output)
+    BuiltIn().should_be_true((arg1, arg2, r) in temp)
