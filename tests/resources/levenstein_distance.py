c128b284333828e232b28f1f59b959205f3a483b
Oleksandr Zelentsov <oleksandrzelentsov@gmail.com>
2016-11-13 19:37:16 +0100

    started writing tests for file input

--- tests/resources/levenstein_distance.py
+++ tests/resources/levenstein_distance.py
@@ -9,10 +9,17 @@ _log = BuiltIn().log
 _log_to_console = BuiltIn().log_to_console
 
 
-@keyword('application returns')
+@keyword('levenstein distance equals')
 def application_returns(arg1, arg2, r):
     _log_to_console('checking if LD({}, {}) = {}'.format(arg1, arg2, r))
     output = (check_output([_get_robot_var('${APP_EXECUTABLE_NAME}'), arg1, arg2])).decode('utf-8')
     output = output.replace(' ', '').replace('\n', '')
     temp = findall(r'\(([\w\d]*)\,([\w\d]*)\)\=(\d*)', output)
     BuiltIn().should_be_true((arg1, arg2, r) in temp)
+
+
+@keyword('check file reading capabilities')
+def kw2(words, lengths):
+    with open(_get_robot_var('${DEFAULT_INPUT_FILENAME}')) as fp:
+        fp.writelines(words.split(', '))
+
--- tests/testcases/Main Behavior.robot
+++ tests/testcases/Main Behavior.robot
@@ -1,11 +1,21 @@
 *** Settings ***
 Library  levenstein_distance.py
+Library  OperatingSystem
+
+*** Variables ***
+${DEFAULT_INPUT_FILENAME}  lwords.txt
+${INPUT_FILENAME}          other.lwords.txt
 
 *** Test cases ***
 Basic functionality
     [Documentation]  Checks if the basic comparison works properly
-    [Template]       Application returns
+    [Template]       Levenstein distance equals
     [Setup]          Log to Console  TESTING
     Jurek  Janek     2
     Marek  Marek     0
     Marek  Mareczek  3
+
+File is located nearby, unmarked
+    [Documentation]  Checks the situation when the file is in the folder but is not requested
+    [Template]       Check file reading capabilities
+    Jurek, Marek, Janek  2,2,3
